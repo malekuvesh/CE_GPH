f5c296ac99b2c53880a9eb63c76f545cc1e0935f
uvesh <uveshmalek31305@gmail.com>
2025-03-19 09:51:37 +0530

    Updated hello.py to use waitress for local development

--- hello.py
+++ hello.py
@@ -1,18 +1,12 @@
-import logging
-
-# Set up logging
-logging.basicConfig(level=logging.DEBUG)
-
 from flask import Flask
+from waitress import serve
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    app.logger.debug("Hello route has been hit")
     return "Hello, World!"
 
 if __name__ == "__main__":
-    from waitress import serve
-    app.logger.debug("Starting server")
+    print("Starting server at http://0.0.0.0:8080")
     serve(app, host='0.0.0.0', port=8080)
