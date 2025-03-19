ccec3368060ab8113417bc1d69219467719f7536
uvesh <uveshmalek31305@gmail.com>
2025-03-19 09:43:07 +0530

    Update hello.py and requirements.txt

--- hello.py
+++ hello.py
@@ -1,12 +1,18 @@
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    app.logger.debug("Hello route has been hit")
     return "Hello, World!"
 
 if __name__ == "__main__":
     from waitress import serve
-    print("Starting server at http://0.0.0.0:8080")
+    app.logger.debug("Starting server")
     serve(app, host='0.0.0.0', port=8080)
